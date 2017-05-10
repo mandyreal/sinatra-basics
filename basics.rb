5b2324f76d4ce3dfd93c4cebda2cd75665eba106
Mandy Real <normandy.real@gmail.com>
2017-05-10 22:48:03 +1000

    Added post form  method and view

--- basics.rb
+++ basics.rb
@@ -11,3 +11,12 @@ end
 get '/hello/:name/:city' do
 	"Hello there, #{params[:name]} from #{params[:city].upcase}	"
 end
+
+get '/form' do
+	erb :form
+end
+
+post '/form' do
+	"You submitted : '#{params[:message]}'"
+end
+
