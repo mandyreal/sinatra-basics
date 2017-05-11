212a4e50aab3fa0e47e2de8197d6ab06a497202f
Mandy Real <normandy.real@gmail.com>
2017-05-11 23:24:05 +1000

    add slim views and post method

--- main.rb
+++ main.rb
@@ -7,22 +7,12 @@ get '/' do
   slim :index 
 end
 
-__END__
+get '/:task' do
+  @task = params[:task].split('-').join(' ').capitalize
+  slim :task
+end
 
-@@layout 
-doctype html 
-html
-  head 
-    meta charset="utf-8" 
-    title Just Do It 
-    link rel="stylesheet" media="screen, projection" href="/styles.css" 
-    /[if lt IE 9] 
-      script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" 
-  body 
-    h1 Just Do It 
-    == yield 
-
-@@index 
-h2 My Tasks
-ul.tasks
-  li Get Milk
+post '/' do
+  @task =  params[:task]
+  slim :task
+end
